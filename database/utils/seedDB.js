98551f8aa6716feffe51bd098a7ea8d89e4d2bf2
raymondkwok7877 <rkwok7877@gmail.com>
2022-05-11 03:19:20 -0400

    Added more students

--- database/utils/seedDB.js
+++ database/utils/seedDB.js
@@ -46,9 +46,35 @@ const seedDB = async () => {
 	  gpa: 3.9
 	});
 
+	const dummy_student3 = await Student.create({
+		firstname: "Jim",
+      lastname: "Jhen",
+	  email: "theJimJhen@cuny.hunter.edu",
+	  imageUrl: "https://st.depositphotos.com/1597387/2407/i/950/depositphotos_24077379-stock-photo-young-asian-student.jpg",
+	  gpa: 4.0
+	});
+
+	const dummy_student4 = await Student.create({
+		firstname: "Dnomyar",
+      lastname: "Kowk",
+	  email: "DnomyarKowk@cuny.queens.edu",
+	  imageUrl: "https://previews.123rf.com/images/tunagaga/tunagaga1209/tunagaga120900063/15335267-young-handsome-asian-student-thinking-and-smile-in-outdoor.jpg",
+	  gpa: 3.2
+	});
+
+	const dummy_student5 = await Student.create({
+		firstname: "Gwei",
+      lastname: "Lo",
+	  email: "gweilo@cuny.queens.edu",
+	  imageUrl: "https://news.virginia.edu/sites/default/files/article_image/seas2.jpg",
+	  gpa: 1.0
+	});
+
 	// Add students to campuses
 	await dummy_student.setCampus(dummy_campus);
 	await dummy_student2.setCampus(dummy_campus2);
+	await dummy_student3.setCampus(dummy_campus);
+	await dummy_student4.setCampus(dummy_campus2);
 }
 
 // Export the database seeding function
